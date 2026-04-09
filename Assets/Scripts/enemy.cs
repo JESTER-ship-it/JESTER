d3da4293c7771c17d18e13dd7a79614ba0985569
JESTER <jthatjustice@gmail.com>
2026-04-09 18:05:43 +0900

    JESTER ENEMY
    123

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,42 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+
+    private Transform target;
+
+    void Start()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+
+        if (player != null)
+        {
+            target = player.transform;
+        }
+    }
+
+    //Update is called once per frame
+
+    void Update()
+    {
+        if (target == null) return;
+        
+        Vector3 direction = target.position - transform.position;
+        direction.y = 0f;
+
+        Vector3 moveDir = direction.normalized;
+
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        if(moveDir !=Vector3.zero)
+        {
+            transform.forward = moveDir;
+        }
+    }
+
+
+
+
+}
